b26500fd6f55ae7098429f41454c0bea8b8529bb
杨兆雨 <50061726+yzydeveloper@users.noreply.github.com>
2022-10-11 11:27:28 +0800

    feat: add comment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const { createVuePlugin } = require('vite-plugin-vue2')
 
+// Suppose this is a dependency that is not packaged and has been published
 fs.cpSync('packages/ui-components', 'node_modules/@ui/components', { recursive: true })
 
 module.exports = {
